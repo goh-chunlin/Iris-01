eb0689b38d1e9c38fe9186b5013ea3c9caadb3d9
Goh Chun Lin <chunlin.goh@haulio.io>
2018-03-18 21:48:30 +0800

    Added _Error.html.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/kataras/iris"
+	"github.com/kataras/iris/context"
 	"github.com/kataras/iris/mvc"
 
 	"personal-projects/iris-01/web/controllers"
@@ -13,12 +14,14 @@ func main() {
 	app.Logger().SetLevel("debug")
 
 	// Load the template files.
-	app.RegisterView(iris.HTML("./web/views", ".html").Layout("shared/_layout.html"))
+	app.RegisterView(iris.HTML("./web/views", ".html").Layout("shared/_Layout.html"))
 
 	// Serve our controllers.
 	mvc.New(app.Party("/home")).Handle(new(controllers.HomeController))
 
 	app.StaticWeb("/public", "./web/wwwroot")
+
+	app.OnAnyErrorCode(onError)
 
 	app.Run(
 		// Start the web server at localhost:8080
@@ -30,4 +33,14 @@ func main() {
 		// enables faster json serialization and more:
 		iris.WithOptimizations,
 	)
+}
+
+type err struct {
+	Title string
+	Code  int
+}
+
+func onError(ctx context.Context) {
+	ctx.ViewData("", err{"Error", ctx.GetStatusCode()})
+	ctx.View("shared/_Error.html")
 }
